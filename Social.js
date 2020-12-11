82a3a392d663d9a2cc0989e30b3f799182909663
Seema Yousuf Zai <72159369+webprogrammer124@users.noreply.github.com>
2020-12-11 12:36:34 +0500

    Add files via upload

---
+++ Social.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Social extends Component {
+    render() {
+        return (
+            <div class="social">
+            <a href="" target="_blank" rel=""><i class="fab fa-codepen"></i></a>
+            <a href="" target="_blank" rel=""><i class="fab fa-github"></i></a>
+            <a href="" target="_blank" rel=""><i class="fab fa-instagram"></i></a>
+            <a href="" target="_blank" rel=""><i class="fab fa-facebook-f"></i></a>
+            </div>
+            )
+        }
+    }
+    
+export default Social
+    
